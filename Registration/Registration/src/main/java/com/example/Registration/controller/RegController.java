5e7c33ad144559f23954d49c820f73887a0ca7af
Madhan-D-01 <madhandamu08@gmail.com>
2026-06-18 17:07:15 +0530

    Initial Commit

---
+++ Registration/Registration/src/main/java/com/example/Registration/controller/RegController.java
@@ -0,0 +1,38 @@
+package com.example.Registration.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Registration.model.User;
+import com.example.Registration.service.RegService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
+public class RegController {
+
+	@Autowired
+	private RegService regService;
+
+	@GetMapping("/h")
+	public String hello() {
+		return "Hello welcome!";
+	}
+
+	@PostMapping("/register")
+	ResponseEntity<User> createregister(@RequestBody User user) {
+		User create = regService.createUser(user);
+		return new ResponseEntity<User>(create, HttpStatus.CREATED);
+	}
+}
---
+++ Registration/Registration/src/main/java/com/example/Registration/controller/package-info.java
@@ -0,0 +1,1 @@
+package com.example.Registration.controller;
